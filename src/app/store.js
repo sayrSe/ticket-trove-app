077a2c1e3a26791b1c71503042da4a8166176d79
Daphne Balane <balanedaphne20@gmail.com>
2023-09-05 18:52:39 +0800

    feat: cinema reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cinemaReducer from '../components/reducers';
 import movieListReducer from "../components/movieListSlice.js";
 import showTimeListReducer from "../components/showTimeListSlice.js";
 
 export const store = configureStore({
-    reducer:{
-        movie: movieListReducer,
-        showtime: showTimeListReducer,
-    }
-})
+  reducer: {
+    movie: movieListReducer,
+    showtime: showTimeListReducer,
+    cinema: cinemaReducer
+  }
+});
