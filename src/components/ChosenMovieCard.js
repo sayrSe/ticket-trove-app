eefd98bcb7cea130d4ea83254c68a57209078df4
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-09-05 17:08:22 +0800

    feat: added component for chosen movie details

---
+++ src/components/ChosenMovieCard.js
@@ -0,0 +1,40 @@
+import { Box, Typography } from '@mui/material';
+
+const boxStyle={
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: '0 auto',
+    width: {
+        xs: 1,
+        md: 500,
+    }
+}
+
+const textStyle={
+    alignSelf: 'start',
+    marginLeft: 2,
+}
+
+const imageStyle={
+    width: "100%",
+    height: "auto",
+    maxWidth: 400
+}
+
+const ChosenMovieCard = (props) => {
+    return(
+        <Box sx={boxStyle}>
+            <Typography variant="h5" sx={textStyle} component="h4" style={{ fontWeight: "bold" }}>{props.movie.title}</Typography>
+            <Typography variant="h8" sx={textStyle} component="h8">{props.movie.release} ◦ {props.movie.rating}  ◦ {props.movie.runtime} minutes</Typography>
+            <Box
+                        component="img"
+                        sx={imageStyle}
+                        src={props.movie.poster}
+                        alt="Movie Poster"
+            />
+        </Box>
+    )
+}
+
+export default ChosenMovieCard;
