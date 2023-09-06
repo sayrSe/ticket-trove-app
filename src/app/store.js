e0de77f76ac929712311c578314ea8dececef0ee
Daphne Balane <balanedaphne20@gmail.com>
2023-09-06 22:07:01 +0800

    feat: added dateOptionsSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,14 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cinemaReducer from '../components/reducers';
+import cinemaSlice from '../components/cinemaReducers';
 import movieListReducer from "../components/movieListSlice.js";
 import showTimeListReducer from "../components/showTimeListSlice.js";
 import seatLayoutReducer from "../components/seatLayoutSlice.js";
+import dateOptionsSlice from '../components/dateOptionsReducer';
 
 export const store = configureStore({
   reducer: {
     movie: movieListReducer,
     showtime: showTimeListReducer,
-    cinema: cinemaReducer,
+    cinemas: cinemaSlice,
+    dateOptions: dateOptionsSlice,
     seat: seatLayoutReducer
   }
 });
