4be95033447fcd65a5bff0463d85ab1f35d1f388
Daphne Balane <balanedaphne20@gmail.com>
2023-09-06 22:09:45 +0800

    refactor: fixed view location render data

--- src/components/ViewLocations.js
+++ src/components/ViewLocations.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { selectCinema, fetchCinemas, getCinemaDetails } from './actions';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { fetchDateOptions, selectCinema, fetchCinemas, getCinemaDetails } from './actions';
+import { useMovies } from "../hooks/useMovies";
 import {
   List,
   ListItem,
@@ -9,53 +10,54 @@ import {
   Button,
   IconButton,
   FormControl,
-  Select,
-  MenuItem,
+  Dialog,
+  DialogContent,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from '@mui/material';
-import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import CircleIcon from '@mui/icons-material/Circle';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 const ViewLocations = () => {
-  //const cinemas = useSelector((state) => state.cinemas);
-  //const selectedCinema = useSelector((state) => state.selectedCinema);
+  const dateOptions = useSelector((state) => state.dateOptions);
+  const cinemas = useSelector((state) => state.cinemas);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const movieInfo = {
-    name: 'Barbie',
-    releaseYear: '2023',
-    ageRating: 'PG',
-    length: '120 minutes',
-    moviePosterUrl: 'https://example.com/movie1.jpg'
-  }
-
-  const cinemas = [
-    {
-      id: 1,
-      address: 'Address 1',
-    },
-    {
-      id: 2,
-      address: 'Address 2',
-    },
-  ];
-
-  const selectedCinema = {
-    id: 1,
-    address: 'Address 1',
+  const location = useLocation();
+  const movieInfo = location.state && location.state.movieInfo;
+  const movieId = location.state && location.state.movieId;
+  const formatDate = (dateString) => {
+    if (!dateString) return '';
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString(undefined, options);
   };
 
   const [selectedDate, setSelectedDate] = useState('');
   const [selectedLocation, setSelectedLocation] = useState('');
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const { findMovie } = useMovies();
 
   useEffect(() => {
+    findMovie(movieId)
+  }, [movieId]);
+
+  
+  useEffect(() => {
+    dispatch(fetchDateOptions(movieId));
     dispatch(fetchCinemas());
   }, [dispatch]);
 
-  // if (!cinemas || cinemas.length === 0) {
-  //   return <div>Loading...</div>;
-  // }
+  if (!cinemas || cinemas.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   const handleCinemaClick = (cinema) => {
+    dispatch(selectCinema(cinema));
+    dispatch(getCinemaDetails(cinema.id));
+    
     setSelectedLocation(cinema.id);
   };
 
@@ -71,29 +73,60 @@ const ViewLocations = () => {
     }
   };
 
+  const handleDateChange = (e) => {
+    setSelectedDate(e.target.value);
+    setDialogOpen(false);
+  };
+
+  const handleOpenDialog = () => {
+    setDialogOpen(true);
+  }
+
   const grayButtonStyle = {
     backgroundColor: 'gray',
     color: 'white',
   }
-  
 
+  const blackButtonStyle = {
+    borderColor: 'black',
+    color: 'black',
+  };
+  
   return (
     <div>
       <div className="movie-poster">
-        <h3>{movieInfo.name}</h3>
-        <p>{movieInfo.releaseYear}&nbsp;{movieInfo.ageRating}&nbsp;{movieInfo.length}</p>
-        <img src={movieInfo.moviePosterUrl} alt="Movie Poster" />
+        {movieInfo && (
+            <>
+              <h3>{movieInfo.title}</h3>
+              <p>{movieInfo.release}&nbsp;{movieInfo.rating}&nbsp;{movieInfo.runtime}</p>
+              <img src={movieInfo.poster} alt="Movie Poster" />
+            </>
+          )}
       </div>
       <FormControl>
         <h4>Select Date</h4>
-        <Select
-          value={selectedDate}
-          onChange={(e) => setSelectedDate(e.target.value)}
-        >
-          <MenuItem value="today">Today</MenuItem>
-          <MenuItem value="tomorrow">Tomorrow</MenuItem>
-          <MenuItem value="dayAfterTomorrow">Day After Tomorrow</MenuItem>
-        </Select>
+        <Button 
+          variant="outlined" 
+          onClick={handleOpenDialog} 
+          endIcon={<KeyboardArrowDownIcon />}
+          style={blackButtonStyle}>
+          {formatDate(selectedDate)}
+        </Button>
+        <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+          <DialogContent>
+            <RadioGroup value={selectedDate} onChange={handleDateChange}>
+              {dateOptions.map((dateOption) => (
+                <FormControlLabel
+                  key={dateOption.id}
+                  value={dateOption.startTime}
+                  control={<Radio />}
+                  label={formatDate(dateOption.startTime)}
+                  style={{ flexDirection: 'row-reverse' }}
+                />
+              ))}
+            </RadioGroup>
+          </DialogContent>
+        </Dialog>
       </FormControl>
       <h4>Select Location</h4>
       <List>
@@ -105,12 +138,16 @@ const ViewLocations = () => {
             selected={selectedLocation === cinema.id}
           >
             <ListItemText
-              secondary={`Address: ${cinema.address}`}
+              primary={`${cinema.name}`}
             />
-            {selectedLocation === cinema.id && (<IconButton color="primary"><ArrowForwardIcon style={{ color: 'yellow'}}/></IconButton>)}
+            {selectedLocation === cinema.id && (
+              <IconButton color="primary" >
+                <CircleIcon style={{ color: '#F2B000'}} />
+              </IconButton>
+            )}
           </ListItem>
         ))}
-      </List>
+      </List>     
       <Button
         variant="contained"
         onClick={handleGoBack}
@@ -125,71 +162,15 @@ const ViewLocations = () => {
         variant="contained"
         color="primary"
         onClick={handleViewShowtimes}
-        disabled={!selectedLocation}
+        disabled={!(selectedLocation && selectedDate)}
+        style={{
+          backgroundColor: selectedLocation && selectedDate ? '#00A4BD' : 'gray',
+          color: selectedLocation && selectedDate ? '#FAFAFA' : 'white',}}
       >
         View Showtimes
-      </Button>
+      </Button>      
     </div>
   );
 };
-
-// return (
-//   <div>
-//       <h2>View Locations</h2>
-//       <div className="movie-poster">
-//         {/* Movie poster here */}
-//         <img src={selectedCinema ? selectedCinema.moviePosterUrl : ''} alt="Movie Poster" />
-//       </div>
-//       <FormControl>
-//         <InputLabel>Select Date</InputLabel>
-//         <Select
-//           value={selectedDate}
-//           onChange={(e) => setSelectedDate(e.target.value)}
-//         >
-//           {/* Date options here */}
-//           <MenuItem value="today">Today</MenuItem>
-//           <MenuItem value="tomorrow">Tomorrow</MenuItem>
-//           <MenuItem value="dayAfterTomorrow">Day After Tomorrow</MenuItem>
-//         </Select>
-//       </FormControl>
-//       <List>
-//         {cinemas.map((cinema) => (
-//           <ListItem
-//             key={cinema.id}
-//             button
-//             onClick={() => handleCinemaClick(cinema)}
-//             selected={selectedLocation === cinema.id}
-//           >
-//             <ListItemText
-//               primary={cinema.name}
-//               secondary={`Address: ${cinema.address}`}
-//             />
-//             {selectedLocation === cinema.id && (
-//               <IconButton color="primary">
-//                 <ArrowForwardIcon />
-//               </IconButton>
-//             )}
-//           </ListItem>
-//         ))}
-//       </List>
-//       <Button
-//         variant="contained"
-//         color="primary"
-//         onClick={handleGoBack}
-//       >
-//         <ArrowBackIcon />
-//         Go Back
-//       </Button>
-//       <Button
-//         variant="contained"
-//         color="primary"
-//         onClick={handleViewShowtimes}
-//       >
-//         View Showtimes
-//         <ArrowForwardIcon />
-//       </Button>
-//     </div>
-  
-// );
 
 export default ViewLocations;
