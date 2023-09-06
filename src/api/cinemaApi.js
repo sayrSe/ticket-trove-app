303ede2763c8c35c102642f4a966a2936032df46
Daphne Balane <balanedaphne20@gmail.com>
2023-09-06 22:06:14 +0800

    refactor: fixed cinema id

--- src/api/cinemaApi.js
+++ src/api/cinemaApi.js
@@ -4,6 +4,10 @@ export const getCinemas = () => {
   return api.get("/cinemas");
 };
 
-export const getCinemaDetails = (cinemaId) => {
-  return api.get(`/cinemas/${cinemaId}`);
+export const getCinemaDetails = (id) => {
+  return api.get(`/cinemas?${id}`);
+};
+
+export const getDate = (movie_id) => {
+  return api.get(`/movies/showtime/${movie_id}`);
 };
