a380d9b5ad0b57d2944c170b36ba30c978dd1c0d
Daphne Balane <balanedaphne20@gmail.com>
2023-09-06 22:07:30 +0800

    refactor: fix fetching cinemas and date

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,32 +1,47 @@
 import * as cinemaApi from '../api/cinemaApi';
+import { setDateOptions } from './dateOptionsReducer';
+import { setCinemas } from './cinemaReducers';
+
+export const FETCH_ERROR = 'FETCH_ERROR';
 
 export const selectCinema = (cinema) => ({
   type: 'SELECT_CINEMA',
   payload: cinema,
 });
 
-export const fetchCinemas = () => {
-  return async (dispatch) => {
+export const fetchError = (error) => ({
+  type: FETCH_ERROR,
+  payload: error,
+});
+
+export const fetchDateOptions = (movie_id) => async (dispatch) => {
     try {
-      const response = await cinemaApi.getCinemas();
-      const cinemas = response.data;
-      // Dispatch an action to update state with the fetched cinemas.
-      dispatch({ type: 'FETCH_CINEMAS', payload: cinemas });
+      const response = await cinemaApi.getDate(movie_id);
+      const dateOptions = response.data;
+      dispatch(setDateOptions(dateOptions));
     } catch (error) {
-      // Handle errors here.
+      console.error('Error fetching date options:', error);
     }
-  };
 };
 
-export const getCinemaDetails = (cinemaId) => {
+export const fetchCinemas = () => async (dispatch) => {
+  try {
+    const response = await cinemaApi.getCinemas();
+    const cinemas = response.data;
+    dispatch(setCinemas(cinemas));
+  } catch (error) {
+    console.error('Error fetching cinemas:', error);
+  }
+};
+
+export const getCinemaDetails = (movie_id) => {
   return async (dispatch) => {
     try {
-      const response = await cinemaApi.getCinemaDetails(cinemaId);
+      const response = await cinemaApi.getCinemaDetails(movie_id);
       const cinemaDetails = response.data;
-      // Dispatch an action to update state with the fetched cinema details.
       dispatch({ type: 'FETCH_CINEMA_DETAILS', payload: cinemaDetails });
     } catch (error) {
-      // Handle errors here.
+      console.error('Error fetching cinema details:', error);
     }
   };
 };
