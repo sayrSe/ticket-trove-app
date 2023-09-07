c435a4b372c62b463f946edd4e2c3bdd029e4a7e
yamboni1 <ncyambot.work@gmail.com>
2023-09-07 14:47:21 +0800

    [ST009] feat: added the senior component

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
 import OtpConfirmation from './OtpConfirmation';
+import SeniorCitizen from './SeniorCitizen';
 
 const Confirmation = () => {
     return(
         <>
             <OtpConfirmation />
+            <SeniorCitizen/>
             <Button>Confirm Booking</Button>
         </>
     )
