e8d894cc7d0ed0ec164ca67f62e1a8eec11c5403
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-09-07 16:54:02 +0800

    [ST012] feat: added otpconfirmation component

--- src/components/OtpConfirmation.js
+++ src/components/OtpConfirmation.js
@@ -1,8 +1,84 @@
+import { Box, Typography, TextField, Stack, Button } from '@mui/material';
+import { useState} from 'react';
+
+const containerStyle={
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '0 auto',
+    marginBottom: 10
+}
+
+const headerStyle={
+    marginLeft: 2,
+    alignSelf: 'flex-start',
+    textAlign: 'left',
+    fontWeight: 'bold'
+}
+
+const phoneInputStyle={
+    marginLeft: 2,
+    width: {
+        xs: 0.7,
+        sm: 350,
+    },
+}
+
+const sendButtonStyle={
+    backgroundColor: '#00A4BD',
+    color: '#FAFAFA',
+    textTransform: 'none',
+    '&:hover':{
+        backgroundColor: '#F2B000',
+        textDecoration: 'underline',
+        fontWeight: 'bold'
+    },
+    width: {
+        xs: 0.2,
+        sm: 100,
+    }
+}
+
+const otpInputStyle={
+    marginLeft: 2,
+    width: {
+        xs: 0.9,
+        sm: 460,
+    },
+    margin: '0 auto'
+}
+
 const OtpConfirmation = () => {
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [otpNumber, setOtpNumber] = useState('');
+
+    const handleInputNumber = (event) => {
+        if(isValid(event.target.value))
+            setPhoneNumber(event.target.value);
+    }
+
+    const handleInputOtp = (event) => {
+        if(isValid(event.target.value))
+            setOtpNumber(event.target.value);
+    }
+
+    const isValid = (value) => {
+        return /^\d+$/.test(value) || value === '';
+    }
+
     return(
-        <>
-            
-        </>
+        <Box sx={containerStyle}>
+            <Typography variant="h6" sx={headerStyle}>Phone Number:</Typography>
+            <Stack direction="row" spacing={1} justifyContent={'center'}>
+                <Box sx={phoneInputStyle}>
+                    <TextField fullWidth required={true} size={'small'} id="phone-number" variant="outlined" value={phoneNumber} onChange={handleInputNumber}/>
+                </Box>
+                <Button sx={sendButtonStyle} >Send</Button>
+            </Stack>
+            <Typography variant="h6" sx={headerStyle}>Verification Code:</Typography>
+            <Box sx={otpInputStyle}>
+                <TextField fullWidth required={true} size={'small'} id="otp-number" variant="outlined" value={otpNumber} onChange={handleInputOtp}/>
+            </Box>
+        </Box>
     )
 }
 
