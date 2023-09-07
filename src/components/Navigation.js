f6a427925302d6e8fa360fa6a2cfa07541434097
Aidee Criselle Penamora <aidee.criselle.penamora@oocl.com>
2023-09-07 12:53:13 +0800

    [ST011] feat: removed search in nav bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,7 @@
 import { NavLink } from 'react-router-dom'
-import { AppBar, Toolbar, Stack, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Button, Box } from '@mui/material';
 import logo from '../assets/images/logo.png';
 import HomeIcon from '@mui/icons-material/Home';
-import SearchIcon from '@mui/icons-material/Search';
 
 const appBarStyle={
     height: {
@@ -24,6 +23,7 @@ const toolBarStyle = {
         md: 'space-between'
     },
     width: 0.9,
+    margin: '0 auto'
 }
 
 const logoStyle={
@@ -44,10 +44,7 @@ const Navigation = () => {
       <AppBar position='fixed' sx={appBarStyle}>
         <Toolbar sx={toolBarStyle}>
             <Box component="img" src={logo} alt={"Ticket trove logo"} edge='start' aria-label='logo' sx={logoStyle}/>
-            <Stack direction='row' spacing={2}>
-                <Button sx={buttonStyle} component={NavLink} to="/"><HomeIcon />Home</Button>
-                <Button sx={buttonStyle}><SearchIcon />Search</Button>
-            </Stack>
+            <Button sx={buttonStyle} component={NavLink} to="/"><HomeIcon />Home</Button>
         </Toolbar>
       </AppBar>
     );
