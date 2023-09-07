70355b9a34491e24161f4323fc0e1e7a3b7b2714
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-09-08 00:38:31 +0800

    [ST006] refactor: removed unused code

--- src/hooks/useCinemas.js
+++ src/hooks/useCinemas.js
@@ -1,7 +1,6 @@
 import { useDispatch } from "react-redux";
 import * as cinemaApi from "../../src/api/cinemaApi";
 import { setCinemaNames } from "../components/cinemaSlice";
-import { setHallNames } from "../components/hallSlice";
 
 export const useCinemas = () => {
     const dispatch = useDispatch();
@@ -11,13 +10,7 @@ export const useCinemas = () => {
         dispatch(setCinemaNames(response.data));
     }
 
-    async function findHallById(hallId){
-        const response = await cinemaApi.getHallById(hallId);
-        dispatch(setHallNames(response.data))
-    }
-
     return {
         findCinemaById,
-        findHallById,
     }
 }
